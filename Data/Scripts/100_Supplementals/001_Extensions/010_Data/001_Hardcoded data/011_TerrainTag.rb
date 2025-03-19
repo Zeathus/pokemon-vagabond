dbfb0de19f2156123c8c304a2e178cb527cc790f
Zeathus <zeathus@gmail.com>
2025-03-20 00:44:26 +0100

    Minor spacing fix

--- Data/Scripts/100_Supplementals/001_Extensions/010_Data/001_Hardcoded data/011_TerrainTag.rb
+++ Data/Scripts/100_Supplementals/001_Extensions/010_Data/001_Hardcoded data/011_TerrainTag.rb
@@ -84,7 +84,7 @@ GameData::TerrainTag.register({
   :id_number              => 22,
   :shows_dust_particle    => true,
   :battle_environment     => :Rock,
-  :land2_wild_encounters   => true
+  :land2_wild_encounters  => true
 })
 
 GameData::TerrainTag.register({
